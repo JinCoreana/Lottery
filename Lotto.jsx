fdef80b8c74c032cdc2084b51b1b7ef1ba9b8e2e
Seoyeonjin Choi <itmustbejin@gmail.com>
2022-02-24 22:11:13 +0100

    img

--- Lotto.jsx
+++ Lotto.jsx
@@ -37,9 +37,7 @@ const Lotto = () => {
         clearTimeout(v);
       });
     };
-  }, [timeouts.current]); // 빈 배열이면 componentDidMount와 동일
-  // 배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
-
+  }, [timeouts.current]); 
   useEffect(() => {
   }, [winNumbers]);
 
